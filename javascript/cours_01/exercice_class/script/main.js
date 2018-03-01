e15279ead0de9445a5ffe8ca6c5cbfa406c4f1ff
Taciturne4463 <immers.charlie@gmail.com>
2018-03-01 15:21:47 +0100

    Remédiation ok

--- javascript/cours_01/exercice_class/script/main.js
+++
@@ -1,6 +0,0 @@
-var buttonImage = document.querySelector('button');
-var imageContainer = document.querySelector('.image-container');
-
-buttonImage.addEventListener('click', function(e) {
-	imageContainer.classList.toggle('image-container--02');
-});
